e8b51840946c49b957472a010ecbf6a38d4020a7
Xcorpia <sergnunes@gmail.com>
2024-05-22 21:00:24 +0200

    Update Invoice Adapter

--- app/src/main/java/com/warrantymanager/InvoiceAdapter.kt
+++ app/src/main/java/com/warrantymanager/InvoiceAdapter.kt
@@ -1,11 +1,16 @@
 package com.warrantymanager
 
 import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.google.firebase.firestore.DocumentReference
 import com.warrantymanager.databinding.ItemInvoiceBinding
 import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
 import java.util.Locale
 
 class InvoiceAdapter(
@@ -35,13 +40,35 @@ class InvoiceAdapter(
             invoiceRef.get().addOnSuccessListener { documentSnapshot ->
                 val invoice = documentSnapshot.toObject(Invoice::class.java)
                 if (invoice != null) {
+                    binding.textViewManufacturer.text = invoice.manufacturer
                     binding.textViewProductName.text = invoice.productName
-                    binding.textViewPrice.text = invoice.price.toString()
-                    binding.textViewPurchaseDate.text = dateFormat.format(invoice.purchaseDate)
-                    binding.textViewWarrantyDate.text = dateFormat.format(invoice.warrantyDate)
+                    binding.textViewPurchaseDate.text = "Fecha de compra: ${dateFormat.format(invoice.purchaseDate)}"
+                    binding.textViewWarrantyDate.text = "Garantía hasta: ${dateFormat.format(invoice.warrantyDate)}"
+
+                    val currentDate = Date()
+                    val remainingMonths = getRemainingWarrantyMonths(currentDate, invoice.warrantyDate)
+                    binding.textViewRemainingWarranty.text = "Quedan: $remainingMonths meses"
+
+                    Glide.with(binding.root.context)
+                        .load(invoice.productImageUrl)
+                        .into(binding.imageViewProduct)
                 }
             }
             itemView.setOnClickListener { onItemClickListener(invoiceRef) }
+        }
+
+        private fun getRemainingWarrantyMonths(currentDate: Date, warrantyDate: Date): Int {
+            val calendar = Calendar.getInstance()
+            calendar.time = currentDate
+            val currentMonth = calendar.get(Calendar.MONTH)
+            val currentYear = calendar.get(Calendar.YEAR)
+
+            calendar.time = warrantyDate
+            val warrantyMonth = calendar.get(Calendar.MONTH)
+            val warrantyYear = calendar.get(Calendar.YEAR)
+
+            val monthsDiff = (warrantyYear - currentYear) * 12 + (warrantyMonth - currentMonth)
+            return if (monthsDiff <= 0) 0 else monthsDiff
         }
     }
 }
